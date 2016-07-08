d64c4f75ccc56d25c6522901e2355223aeb603ab
antemeridiem <icbfernandez2012@gmail.com>
2016-07-08 15:20:54 +0800

    Add project files.

---
+++ anteRSS/anteRSS.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "resource.h"
+
+#define MSG_TRAY_ICON	WM_APP + 1
---
+++ anteRSS/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <shellapi.h>
+#include <CommCtrl.h>
+
+#include <strsafe.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: reference additional headers your program requires here
