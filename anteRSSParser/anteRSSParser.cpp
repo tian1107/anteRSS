9aa42192b1430347d0ca8a5392957e0874ac5005
antemeridiem <icbfernandez2012@gmail.com>
2016-07-12 13:23:10 +0800

    prelim rssmanager, add and list feeds

--- anteRSSParser/anteRSSParser.cpp
+++ anteRSSParser/anteRSSParser.cpp
@@ -25,6 +25,41 @@ namespace anteRSSParser
 		return std::string();
 	}
 
+	std::string RSSItem::getUniqueId()
+	{
+		if (format == RSSFormat::INVALID)
+			return "invalid";
+
+		if (format == RSSFormat::RSS2)
+		{
+			tinyxml2::XMLElement * guid;
+			if (guid = asXML->FirstChildElement("guid"))
+				return guid->GetText();
+			else
+				// TODO generate a deterministic guid
+				return "no guid";
+		}
+
+		return std::string();
+	}
+
+	std::string RSSItem::getDescription()
+	{
+		if (format == RSSFormat::INVALID)
+			return "invalid";
+
+		if (format == RSSFormat::RSS2)
+		{
+			tinyxml2::XMLElement * desc;
+			if (desc = asXML->FirstChildElement("description"))
+				return desc->GetText();
+			else
+				return "no guid";
+		}
+
+		return std::string();
+	}
+
 	RSSItem RSSItem::getNext()
 	{
 		if (format == RSSFormat::RSS2)
@@ -130,4 +165,60 @@ namespace anteRSSParser
 		std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
 		return myconv.from_bytes(str);
 	}
+
+	RSSManager::RSSManager(std::string dbFile)
+	{
+		int rc = sqlite3_open(dbFile.c_str(), &db);
+		addFeedStmt = nullptr;
+
+		// TODO error checking here
+		if (rc) {
+			//OutputDebugString((std::wstring(L"Can't open database") + anteRSSParser::convertToWide(sqlite3_errmsg(db)) + L"\n").c_str());
+			return;
+		}
+		else {
+			//OutputDebugString(L"Opened it success!\n");
+		}
+
+		std::string feedStr = "insert into FeedInfo (name, url) values (?1, ?2);";
+		rc = sqlite3_prepare_v2(db, feedStr.c_str(), feedStr.length() + 1, &addFeedStmt, NULL);
+		feedStr = "select name, url from FeedInfo;";
+		rc = sqlite3_prepare_v2(db, feedStr.c_str(), feedStr.length() + 1, &getAllFeedsStmt, NULL);
+	}
+
+	RSSManager::~RSSManager()
+	{
+		sqlite3_finalize(addFeedStmt);
+		sqlite3_finalize(getAllFeedsStmt);
+		sqlite3_close(db);
+	}
+
+	void RSSManager::addFeed(RSSFeed feed)
+	{
+		sqlite3_clear_bindings(addFeedStmt);
+		sqlite3_bind_text(addFeedStmt, 1, feed.name.c_str(), -1, SQLITE_STATIC);
+		sqlite3_bind_text(addFeedStmt, 2, feed.url.c_str(), -1, SQLITE_STATIC);
+		int rc = sqlite3_step(addFeedStmt);
+		sqlite3_reset(addFeedStmt);
+	}
+
+	RSSFeedVector RSSManager::getAllFeeds()
+	{
+		std::vector<RSSFeed> result;
+
+		int rc = sqlite3_step(getAllFeedsStmt);
+		while (rc == SQLITE_ROW)
+		{
+			RSSFeed feed;
+			feed.name = (const char *) sqlite3_column_text(getAllFeedsStmt, 0);
+			feed.url = (const char *) sqlite3_column_text(getAllFeedsStmt, 1);
+			result.push_back(feed);
+			rc = sqlite3_step(getAllFeedsStmt);
+		}
+
+		sqlite3_reset(getAllFeedsStmt);
+
+		return result;
+	}
+
 }
--- anteRSSParser/anteRSSParser.h
+++ anteRSSParser/anteRSSParser.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <vector>
+#include <map>
+#include <string>
+#include <sqlite3.h>
+
 #include "tinyxml2\tinyxml2.h"
 
 namespace anteRSSParser
@@ -19,20 +24,55 @@ namespace anteRSSParser
 	public:
 		RSSItem(RSSFormat format, tinyxml2::XMLElement * element);
 		std::string getTitle();
+		std::string getUniqueId();
+		std::string getDescription();
 		RSSItem getNext();
 		bool isInvalid();
 	};
 
 	class RSSDocument : public tinyxml2::XMLDocument 
 	{
+		friend class RSSManager;
 	private:
 		RSSFormat format;
+		bool cached;
+		std::map<std::string, RSSItem> itemCache;
 		bool determineFormat();
 	public:
 		RSSDocument(bool processEntities = true, tinyxml2::Whitespace ws = tinyxml2::PRESERVE_WHITESPACE);
 		std::string getTitle();
 		RSSItem getFirstItem();
 		void reset(bool makeEmpty);
+	};
+
+	// gets called when a feed is updated, would be in a different thread
+	typedef void(*RSSManagerCallback)(std::string url, bool success);
+
+	struct RSSFeed
+	{
+		std::string name;
+		std::string url;
+	};
+	typedef std::vector<RSSFeed> RSSFeedVector;
+
+	class RSSManager
+	{
+	private:
+		sqlite3* db;
+		sqlite3_stmt * addFeedStmt;
+		sqlite3_stmt * getAllFeedsStmt;
+		std::map<std::string, RSSDocument> documentCache;
+	public:
+		RSSManager(std::string dbFile);
+		~RSSManager();
+		void addFeed(RSSFeed feed);
+		RSSFeedVector getAllFeeds();
+		void removeFeed(RSSFeed feed);
+		void updateFeed(RSSFeed feed, RSSManagerCallback callback);
+		void updateAll(RSSManagerCallback callback);
+		void markAsRead(std::string guid, bool read);
+		void markAsArchive(std::string guid, bool archive);
+		std::string getLastError();
 	};
 
 	// Converts utf8 strings to wstring
