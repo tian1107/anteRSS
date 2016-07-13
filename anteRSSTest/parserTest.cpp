af4382fac1463662fb853e7f24e952148c6ba168
antemeridiem <icbfernandez2012@gmail.com>
2016-07-13 14:22:57 +0800

    removefeedtest, in memory database

--- anteRSSTest/parserTest.cpp
+++ anteRSSTest/parserTest.cpp
@@ -14,13 +14,12 @@ namespace anteRSSTest
 		RSSManager * manager;
 		RSSManagerTest()
 		{
-			manager = new RSSManager("test.db");
+			manager = new RSSManager(":memory:");
 		}
 
 		~RSSManagerTest()
 		{
 			delete manager;
-			remove("test.db");
 		}
 
 		TEST_METHOD(addFeedTest)
@@ -60,6 +59,39 @@ namespace anteRSSTest
 			Assert::AreEqual(feed2.url, resultFeed2.url, L"url not preserved", LINE_INFO());
 			Assert::AreNotEqual(feed2.id, resultFeed2.id, L"no new id", LINE_INFO());
 			Assert::AreNotEqual(resultFeed.id, resultFeed2.id, L"no new id", LINE_INFO());
+		}
+
+		TEST_METHOD(removeFeedTest)
+		{
+			RSSFeed feed;
+			feed.id = 0;
+			feed.name = "testtitle 日本語できるかな？　";
+			feed.url = "testurl";
+
+			manager->addFeed(feed);
+
+			RSSFeed feed2;
+			feed2.id = 0;
+			feed2.name = "あたらしい！";
+			feed2.url = "testurl2";
+
+			manager->addFeed(feed2);
+
+			RSSFeedVector result = manager->getAllFeeds();
+
+			Assert::AreEqual(2, (int)result.size(), L"feeds added amount mismatch", LINE_INFO());
+
+			manager->removeFeed(result.at(0).id);
+			manager->removeFeed(11);
+
+			RSSFeedVector result2 = manager->getAllFeeds();
+
+			Assert::AreEqual(1, (int)result2.size(), L"feeds removed amount mismatch", LINE_INFO());
+
+			RSSFeed resultFeed2 = result2.at(0);
+
+			Assert::AreEqual(feed2.name, resultFeed2.name, L"name not preserved", LINE_INFO());
+			Assert::AreEqual(feed2.url, resultFeed2.url, L"url not preserved", LINE_INFO());
 		}
 
 	};
