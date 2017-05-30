cd97acfb4a3b9c5e081453e47bfeb36b6aa705ed
Ian Christian Fernandez <icbfernandez2012@gmail.com>
2017-05-30 20:26:27 +0800

    rssmanager clr wrapper

---
+++ anteRSS cplusnet/RSSManagerWrapper.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "anteRSSParser\RSSManager.h"
+
+namespace anteRSScplusnet
+{
+	public ref class RSSFeedWrapper
+	{
+	private:
+		anteRSSParser::RSSFeed * content;
+	public:
+		RSSFeedWrapper(anteRSSParser::RSSFeed * content);
+
+		property System::String ^Name
+		{
+			System::String ^get();
+		}
+	};
+
+	public ref class RSSManagerWrapper
+	{
+	private:
+		anteRSSParser::RSSManager * manager;
+		anteRSSParser::RSSFeedVector * feedCache;
+	public:
+		RSSManagerWrapper(System::String^ dbFile);
+
+		System::Void updateFeedListCache();
+		RSSFeedWrapper ^ getFeedListCacheAt(System::Int32 index);
+		System::Int32 getFeedListCacheLength();
+
+		!RSSManagerWrapper();
+		~RSSManagerWrapper();
+	};
+
+}
+
