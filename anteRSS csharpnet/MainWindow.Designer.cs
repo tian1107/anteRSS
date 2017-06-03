2ccd21550d2cb84bb0110e7429affeeb76c0eee7
Ian Christian Fernandez <icbfernandez2012@gmail.com>
2017-06-03 16:43:04 +0800

    csharpnet

---
+++ anteRSS csharpnet/MainWindow.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace anteRSS_csharpnet
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topBar = new System.Windows.Forms.MenuStrip();
+            this.buttonBar = new System.Windows.Forms.ToolStrip();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitLeftRight = new System.Windows.Forms.SplitContainer();
+            this.splitUpDown = new System.Windows.Forms.SplitContainer();
+            this.topBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitLeftRight)).BeginInit();
+            this.splitLeftRight.Panel2.SuspendLayout();
+            this.splitLeftRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitUpDown)).BeginInit();
+            this.splitUpDown.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topBar
+            // 
+            this.topBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.topBar.Location = new System.Drawing.Point(0, 0);
+            this.topBar.Name = "topBar";
+            this.topBar.Size = new System.Drawing.Size(784, 24);
+            this.topBar.TabIndex = 0;
+            this.topBar.Text = "topBar";
+            // 
+            // buttonBar
+            // 
+            this.buttonBar.Location = new System.Drawing.Point(0, 24);
+            this.buttonBar.Name = "buttonBar";
+            this.buttonBar.Size = new System.Drawing.Size(784, 25);
+            this.buttonBar.TabIndex = 1;
+            this.buttonBar.Text = "buttonBar";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // splitLeftRight
+            // 
+            this.splitLeftRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitLeftRight.Location = new System.Drawing.Point(0, 49);
+            this.splitLeftRight.Name = "splitLeftRight";
+            // 
+            // splitLeftRight.Panel2
+            // 
+            this.splitLeftRight.Panel2.Controls.Add(this.splitUpDown);
+            this.splitLeftRight.Size = new System.Drawing.Size(784, 490);
+            this.splitLeftRight.SplitterDistance = 196;
+            this.splitLeftRight.TabIndex = 3;
+            // 
+            // splitUpDown
+            // 
+            this.splitUpDown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitUpDown.Location = new System.Drawing.Point(0, 0);
+            this.splitUpDown.Name = "splitUpDown";
+            this.splitUpDown.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.splitUpDown.Size = new System.Drawing.Size(584, 490);
+            this.splitUpDown.SplitterDistance = 245;
+            this.splitUpDown.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.splitLeftRight);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.buttonBar);
+            this.Controls.Add(this.topBar);
+            this.MainMenuStrip = this.topBar;
+            this.Name = "MainWindow";
+            this.Text = "anteRSS csharpnet";
+            this.topBar.ResumeLayout(false);
+            this.topBar.PerformLayout();
+            this.splitLeftRight.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitLeftRight)).EndInit();
+            this.splitLeftRight.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitUpDown)).EndInit();
+            this.splitUpDown.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip topBar;
+        private System.Windows.Forms.ToolStrip buttonBar;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitLeftRight;
+        private System.Windows.Forms.SplitContainer splitUpDown;
+    }
+}
+
