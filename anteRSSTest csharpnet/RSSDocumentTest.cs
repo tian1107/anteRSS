f806559f4d36ef1fea7c0974f5bf69e605c71afe
Ian Christian Fernandez <icbfernandez2012@gmail.com>
2018-06-21 18:59:10 +0800

    prelim RSSDocument port

---
+++ anteRSSTest csharpnet/RSSDocumentTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace anteRSSTest_csharpnet
+{
+	[TestClass]
+	public class RSSDocumentTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
